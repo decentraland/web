f0c05dbc2b6d895bba1942fda2ef8fa881f09718
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-07-12 19:01:31 +0200

    First bootstrap versioning to static files without disturbing the file list

--- webpack.browser.js
+++ webpack.browser.js
@@ -1,6 +1,26 @@
 var webpack = require('webpack')
 var path = require('path')
 var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var CopyFilesPlugin = require('copy-webpack-plugin')
+var lastCommitSHA = require('child_process').execSync('git rev-parse HEAD').toString().trim().slice(0, 5)
+
+
+// This is not ideal but rather a fast solution to an anoying problem
+// To be revised
+var copyFiles = new CopyFilesPlugin([{
+    from: '*.html',
+    to: '.',
+    transform: function(content, absolutePath) {
+      if (process.env.NODE_ENV === 'production') {
+        let contentString = content.toString()
+        contentString = contentString.replace(/__RESOURCE_VERSION__/g, lastCommitSHA)
+        content = new Buffer(contentString)
+      }
+
+      return content
+    }
+  }
+])
 
 
 module.exports = {
@@ -31,7 +51,7 @@ module.exports = {
 
   plugins: [
     new ExtractTextPlugin('[name]'),
-
+    copyFiles,
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       output: { comments: false }
