7691701f03fd1dd015c5d5638ff2444bdd161771
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-07-13 15:36:28 +0200

    LAN-9 Build html files. Version static resources

--- webpack.browser.js
+++ webpack.browser.js
@@ -5,24 +5,6 @@ var CopyFilesPlugin = require('copy-webpack-plugin')
 var lastCommitSHA = require('child_process').execSync('git rev-parse HEAD').toString().trim().slice(0, 5)
 
 
-// This is not ideal but rather a fast solution to an anoying problem
-// To be revised
-var copyFiles = new CopyFilesPlugin([{
-    from: '*.html',
-    to: '.',
-    transform: function(content, absolutePath) {
-      if (process.env.NODE_ENV === 'production') {
-        let contentString = content.toString()
-        contentString = contentString.replace(/__RESOURCE_VERSION__/g, lastCommitSHA)
-        content = new Buffer(contentString)
-      }
-
-      return content
-    }
-  }
-])
-
-
 module.exports = {
   devtool: 'cheap-module-source-map',
 
@@ -50,8 +32,23 @@ module.exports = {
   },
 
   plugins: [
+    // This is not ideal but rather a fast solution to version static files via commit hash
+    new CopyFilesPlugin([{
+        from: './views/**/*', to: __dirname, flatten: true,
+        transform: function(content, absolutePath) {
+          if (process.env.NODE_ENV === 'production') {
+            let contentString = content.toString()
+            contentString = contentString.replace(/__RESOURCE_VERSION__/g, lastCommitSHA)
+            content = new Buffer(contentString)
+          }
+
+          return content
+        }
+      }
+    ]),
+
     new ExtractTextPlugin('[name]'),
-    copyFiles,
+
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       output: { comments: false }
